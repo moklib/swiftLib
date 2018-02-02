64660f377b574600023280f4ca9c283e1c8fc401
liam17 <37907909@qq.com>
2018-02-02 17:18:44 +0800

    初稿

---
+++ RobobloqSwiftDemo/RobobloqSwiftDemo/RbLib/Robot.swift
@@ -0,0 +1,35 @@
+//
+//  Robot.swift
+//  RobobloqSwiftDemo
+//
+//  Created by liamios on 2018/1/23.
+//  Copyright © 2018年 Liam. All rights reserved.
+//
+
+import Foundation
+
+public class Robot
+{
+    init(){
+        
+    }
+    
+    /**
+     * @summary 向机器人发送请求
+     * @ignore
+     */
+    func request(isWait:Bool, order:Int, buffer:Data) {
+        
+    }
+    
+    /**
+     * @summary 发送数据
+     * @ignore
+     */
+    func send(buffer:Data) {
+        //return this.request(false, null, buffer);
+    }
+    
+    
+}
+
